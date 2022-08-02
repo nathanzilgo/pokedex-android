e43623051cb84b274a5a6697acabe0326dfdbeed
nathan <npedroza@nelogica.com.br>
2022-08-02 09:09:59 -0300

    add type colors

--- app/src/main/java/com/zilgo/pokedex/domain/PokemonType.kt
+++ app/src/main/java/com/zilgo/pokedex/domain/PokemonType.kt
@@ -1,5 +1,27 @@
 package com.zilgo.pokedex.domain
 
+import com.zilgo.pokedex.R
+
 data class PokemonType (
     val name: String
-)
+) {
+    fun mapColor(): Int {
+        return when (name.lowercase()) {
+            "fire" -> R.color.fire
+            "water" -> R.color.water
+            "poison" -> R.color.poison
+            "electric" -> R.color.electric
+            "ground" -> R.color.ground
+            "bug" -> R.color.bug
+            "ghost" -> R.color.ghost
+            "steel" -> R.color.steel
+            "dark" -> R.color.dark
+            "dragon" -> R.color.dragon
+            "ice" -> R.color.ice
+            "psychic" -> R.color.psychic
+            "grass" -> R.color.grass
+            "flying" -> R.color.flying
+            else -> R.color.white
+        }
+    }
+}
